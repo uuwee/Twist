7aabbd781c1a95120ff5c08adb83cd8fa415ba4b
uwe <40294263+uuwee@users.noreply.github.com>
2025-04-13 09:42:47 +0900

    draw polygon

--- src/main.cpp
+++ src/main.cpp
@@ -27,16 +27,16 @@ int main() {
         pixel->at(2) = 0.5; 
     });
 
-    using Vertex = std::array<Precision, 3>;
+    using Vertex = std::array<Precision, 6>;
     using Index = uint32_t;
     std::vector<Vertex> vertices = {
-        {-0.5, 0.5, 0.0}, 
-        {0.5, 0.5, 0.0}, 
-        {0.5, -0.5, 0.0}, 
-        {-0.5, -0.5, 0.0}
+        {-0.5, 0.5, 0.0, 1.0, 0.0, 0.0}, 
+        {0.5, 0.5, 0.0, 0.0, 1.0, 0.0}, 
+        {0.5, -0.5, 0.0, 0.0, 0.0, 1.0}, 
+        {-0.5, -0.5, 0.0, 0.0, 0.0, 0.0},
     };
     std::vector<Index> indices = {0, 1, 2, 0, 2, 3};
-    std::for_each(std::execution::unseq, pixel_indices, pixel_indices + width * height, [image, vertices, indices](int idx) {
+    std::for_each(std::execution::par_unseq, pixel_indices, pixel_indices + width * height, [image, vertices, indices](int idx) {
         Color* pixel = (image + idx);
         Precision s = static_cast<Precision>(idx / width) / static_cast<Precision>(height) - 0.5f; 
         Precision t = static_cast<Precision>(idx % width) / static_cast<Precision>(width) - 0.5f; 
@@ -60,12 +60,21 @@ int main() {
             }
 
             if (inside) {
-                value = {0.0, 0.0, 0.0};
+                value[0] = alpha * v0[3] + beta * v1[3] + gamma * v2[3];
+                value[1] = alpha * v0[4] + beta * v1[4] + gamma * v2[4];
+                value[2] = alpha * v0[5] + beta * v1[5] + gamma * v2[5];
+
+                break;
             }
         }
 
         *pixel = value;
     });
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration = end - start;
+    start = end;
+    std::cout << "Render in: " << duration.count() << " ms" << std::endl;
 
 
     constexpr Precision max_value = 255.0;
@@ -87,12 +96,12 @@ int main() {
     }
     outFile.close();
 
+    end = std::chrono::high_resolution_clock::now();
+    duration = end - start;
+    std::cout << "Output ppm in: " << duration.count() << " ms" << std::endl;
+
     delete [] pixel_indices;
     delete[] image;
-
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, std::milli> duration = end - start;
-    std::cout << "Render time: " << duration.count() << " ms" << std::endl;
 
     return 0;
 }
